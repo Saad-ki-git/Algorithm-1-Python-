9232404d59f1fbae42f234260d54e648d24defd0
SAADi <93855880+Saad-ki-git@users.noreply.github.com>
2024-02-01 13:32:50 +0500

    Update Percolation threshold.py

--- Percolation threshold.py
+++ Percolation threshold.py
@@ -1,3 +1,11 @@
+#What is monte Carlo Percolation Thresholds
+# To estimate the percolation threshold, consider the following computational experiment:
+# * Initialize all sites to be blocked.
+# * Repeat the following until the system percolates:
+# * Choose a site uniformly at random among all blocked sites.
+# * Open the site.
+# The fraction of sites that are opened when the system percolates provides an estimate of the percolation threshold.
+
 import random
 import statistics
 
